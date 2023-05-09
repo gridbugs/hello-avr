cbd721117fe3b1aebe77c42befa7b6547ee8473c
Stephen Sherratt <stephen@sherra.tt>
2023-05-09 10:37:20 +1000

    Attempt to simplify c code

--- main.c
+++ main.c
@@ -1,31 +1,27 @@
-#include <avr/io.h>
 #include <stdio.h>
+#include <avr/io.h>
 
 #define BAUD 9600
 #define FOSC 16000000
 #define MYUBRR (FOSC/16/BAUD-1)
 
+// Send a character over USART0.
 int USART0_tx(char data, struct __file* _f) {
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = data;
+    while (!(UCSR0A & (1 << UDRE0))); // wait for the data buffer to be empty
+    UDR0 = data; // write the character to the data buffer
     return 0;
 }
 
-int USART0_rx(struct __file* _f) {
-    while (!(UCSR0A & (1 << RXC0)));
-    return UDR0;
-}
-
+// Create a stream associated with transmitting data over USART0 (this will be
+// used for stdout so we can print to a terminal with printf).
 static FILE uartout = FDEV_SETUP_STREAM(USART0_tx, NULL, _FDEV_SETUP_WRITE);
-static FILE uartin =  FDEV_SETUP_STREAM(NULL, USART0_rx, _FDEV_SETUP_READ);
 
 void USART0_init( void ) {
     UBRR0H = (MYUBRR >> 8) & 0xF;
     UBRR0L = MYUBRR & 0xFF;
-    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    UCSR0B = 1 << TXEN0;
     UCSR0C = (1 << USBS0) | (3 << UCSZ00);
     stdout = &uartout;
-    stdin  = &uartin;
 }
 
 typedef struct {
