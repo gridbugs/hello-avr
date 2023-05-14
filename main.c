990efc28d4be408f6fb9efe2756c5aa3638f8b9d
Stephen Sherratt <stephen@sherra.tt>
2023-05-14 14:22:16 +1000

    Reset to just print hello world

--- main.c
+++ main.c
@@ -25,55 +25,8 @@ void USART0_init( void ) {
     stdout = &uartout;
 }
 
-typedef struct {
-    volatile uint8_t* port;
-    uint8_t bit;
-} led_t;
-
-#define N_LEDS 15
-led_t leds[N_LEDS] = {
-    { .port = &PORTD, .bit = 7 },
-    { .port = &PORTD, .bit = 6 },
-    { .port = &PORTD, .bit = 5 },
-    { .port = &PORTD, .bit = 4 },
-    { .port = &PORTD, .bit = 3 },
-    { .port = &PORTD, .bit = 2 },
-    { .port = &PORTB, .bit = 2 },
-    { .port = &PORTB, .bit = 1 },
-    { .port = &PORTB, .bit = 0 },
-    { .port = &PORTC, .bit = 5 },
-    { .port = &PORTC, .bit = 4 },
-    { .port = &PORTC, .bit = 3 },
-    { .port = &PORTC, .bit = 2 },
-    { .port = &PORTC, .bit = 1 },
-    { .port = &PORTC, .bit = 0 },
-};
-
-void led_on(led_t led) {
-    *led.port = 1 << led.bit;
-}
-
-#define N_INDICES 3
-
 int main(void) {
     USART0_init();
     printf("Hello, World!\r\n");
-    DDRB = 0xFF;
-    DDRC = 0xFF;
-    DDRD = 0xFF;
-    int indices[N_INDICES] = {0, 5, 10};
-    while (1) {
-        PORTB = 0x0;
-        PORTC = 0x0;
-        PORTD = 0x0;
-        for (int i = 0; i < N_INDICES; i++) {
-            led_on(leds[indices[i]]);
-            indices[i] = (indices[i] + 1) % N_LEDS;
-        }
-        uint32_t delay = 100000;
-        while (delay > 0) {
-            delay -= 1;
-        }
-    }
     return 0;
 }
